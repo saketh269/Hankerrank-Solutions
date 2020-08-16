8dfc8d394fa2d75bdc487c5851ff364875bc6f60
saketh269 <66899838+saketh269@users.noreply.github.com>
2020-08-16 22:43:57 +0530

    solution

--- Hankerrank solution-Kangaroo/kangaroo.py
+++ Hankerrank solution-Kangaroo/kangaroo.py
@@ -8,19 +8,13 @@ import sys
 
 # Complete the kangaroo function below.
 def kangaroo(x1, v1, x2, v2):
-  k1=x1+v1
-  k2=x2+v2
-  if(k1==k2):
-    return 1
-  else:
-    eq(k1,k2,v1,v2)
-def eq(k1,k2,v1,v2):
-    while(k1!=k2):
-        k1=k1+v1
-        k2=k2+v2
-    return 1
-        
-    
+  for n in range(10000):
+    if((x1+v1)==(x2+v2)):
+      return "YES"
+    x1+=v1
+    x2+=v2
+  return "NO"
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -35,7 +29,7 @@ if __name__ == '__main__':
     v2 = int(x1V1X2V2[3])
 
     result = kangaroo(x1, v1, x2, v2)
-    if result==1:
-      print("YES")
-    else:
-      print("NO")
+
+    fptr.write(result + '\n')
+
+    fptr.close()
